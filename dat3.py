a0f1988eee6f99dd31a88c48e945d09e950415f6
Harshkumar71 <harshkumar0342@gmail.com>
2024-05-28 13:57:13 +0530

    dict

---
+++ dat3.py
@@ -0,0 +1,148 @@
+
+#dt= {
+# "name" :"mohan",
+# "branch" : "cse",
+# "year" :2,
+# "year" :1,
+# "class" :["one","two","three"],
+# "year" :("1","2","3"),
+# "set" :{"true","hello","1"}
+#}
+#print(dt)
+#print(type(dt))
+#print(len())
+#thisdt = dict(name="harsh",section="4,year="2")
+#print(thisdt)
+#print(type(thisdt))
+# dt = {  
+    #    "brand" : "ford",
+    #    "model" : "GT",
+    #    "year" : 1919
+    #  }
+# print(dt["model"])
+# print(dt.get("model"))
+# print(dt.keys())
+# print(dt.values())
+# print(dt.items())
+# x= dt.update({"model :"audi"})
+#print(dt.pop("model"))
+#print(dt)
+# dt = {
+    # 'dt1':{"name":"ram"},
+    # 'dt2':{"name2":"mohan"}
+    # }
+# print(dt)
+# print(dt['dt1'])
+# print(dt['dt2']["name2"])
+ 
+#a= 10 
+#b= 20
+# if b>a:
+#  print("b is graeter then a")   
+#elif a>b:
+#   print("a is greater")  
+#else:
+#     print("b is greater then a")
+# a= 200
+# b= 33
+# if b>a:
+    # print("b is greater")
+# else:
+    # print("a is greater")    
+# a= 2
+# b= 330
+# print("a") if a>b else print("b")
+# a = 330
+# b = 330
+# print("A") if a>b else print("equal") if a==b else print("b")
+# a= 200 
+# b= 33
+# #c= 500
+# if a>b and c>a:
+    # print("both condition are true")
+# 
+# if c>b or c>a:
+    # print("both condition are true")
+# 
+# if  not a>c:
+    # print("c is lower")
+     
+
+# x= 41
+# if x>10:
+    # print("above ten"),
+    #pass
+#    if x>20:
+    # print("above twenty"), 
+    #  else:
+    #    print("but not above twenty")
+
+# user data input
+# x = input("enter alphabet:")
+# y = input("enter name:")
+# if x!=y:
+    # print("cond is true")
+# else:
+    # print("cond is not true")
+
+# print vowel
+# x= input("enter vowel")
+# if x=='a'or x=='e'or x=='i' or x=='o' or x=='u' :
+    # print("is a vowel")
+# else:
+    # print("is not a vowel")
+
+# loops
+#for i in "mango":
+# print(i)
+
+#  fruits= ["apple","mango","cherry"]
+# for x in fruits:
+    #  print(x)
+# if x == "mango":
+        # break
+
+#for x in range(2,10):
+#    print(x)
+
+# for x in range(6):
+    # print(x)
+# else:
+    # print("fnished")
+
+# for x in range(6):
+    # if x==3: break
+    # print(x) 
+# else:
+    # print("finish")
+# 
+# for x in dt:
+    # print(dt[x])
+# for x in dt.keys():
+    # print(x)
+# for x in dt.values():
+    # print(x)
+#for x,yin dt.items():
+ #   print(x,y)
+
+
+# x=input("enter the values")
+# for i in range(1,11):
+#    print(f"(x) * (i)= (x*i)")
+   
+# fruits = ["apple","mango","cherry"]
+# for index, fruits in enumerate(fruits):
+    # print(index,fruits)  
+
+
+
+
+
+
+
+
+
+
+
+
+
