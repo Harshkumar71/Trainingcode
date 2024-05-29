1262c4f83cb37f18b1f86deede75b9158f5febd4
Harshkumar71 <harshkumar0342@gmail.com>
2024-05-29 13:45:54 +0530

    day4.py

---
+++ day4.py
@@ -0,0 +1,142 @@
+# i= 1
+# while i<6:
+    # print(i)
+    # i+=1
+# i=17
+# while i<200:
+    # print(i)
+    # i+=2
+
+
+# i = 1
+# while i<6:
+    # print(i)
+    # if i==3:
+        # break
+    # i+=1
+
+# i = 25
+# while i<50:
+    # print(i)
+    # if i==45:
+    #  break
+    # i+=1
+
+# i= 0
+# while i<6:
+    # i+=1
+    # if i==3:
+    #  continue
+    # print(i)
+ 
+# i = 2
+# while i<30:
+    # i+=1
+    # if i==10:
+    #  continue
+    # print(i)
+
+
+# i=1 
+# while i<6:
+    # print(i)
+    # i+=1
+# else:
+    # print("i is not longer then 6")  
+
+i = 2
+while i<20:
+    print(i)
+    i+=1
+else:
+     print("i is not greater then ten")
+
+
+
+
+# function
+
+# def my_function():
+    # print("hello from a function")
+# my_function()       => (#calling function)
+
+# def my_function1(fname):
+#    print(fname+" " + "sharma")
+# my_function1("mohit")         
+ 
+# num1 = 200
+# num2 = 400
+# result = num1+num2
+#  print(result)
+
+# def my_function2(*kids):
+    # print("the youngest child is " + kids[2])
+# my_function2("ram","mohan","harsh")             
+         
+# def my_country(country="india"):
+    # print("i am from" +country)
+# my_country("india")
+# my_country("london")
+ 
+#def square_element(input_lst):
+#  temp= []
+#  for x in input_lst
+#    temp.append(x**2)
+#  return temp
+ 
+# def average_function(input_lst):
+#   return sum(input_lst)/len(input_lst)
+# x = avgerage_function([23'34'66'67])
+# print(x)
+
+# def min_function(1s):
+    # print(min(1s))
+# min_function([42,56,57])
+
+# def apna_min(lst):
+    # minimum = x[0]
+    # for i in lst:
+    #   if minimum>i:
+        #   minimum = i
+  # return minimum
+  
+#x = lambda a: a+20
+#print(x(40))
+
+# x= lambda a,b:a*b
+# print(x(23,45))
+
+# x= lambda a,b,c:a*b*c
+# print(x(23,56,78))
+
+# def function(n):
+#   return lambda a:a*n
+# double = function(2)
+# print(double(44))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
